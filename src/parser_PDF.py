b5c4f6a339d2d900008834f2cbca92dddc4c67bf
Michal <01187288@pw.edu.pl>
2026-03-25 18:20:05 +0100

    Read content from pdf file to markdown format

---
+++ src/parser_PDF.py
@@ -0,0 +1,20 @@
+import pymupdf4llm
+
+
+class ParserPDF:
+    def __init__(self):
+        pass
+
+    def read_markdown_from_file(self, filepath: str) -> str:
+            return pymupdf4llm.to_markdown(filepath)
+
+
+if __name__ == "__main__":
+    parser = ParserPDF()
+    text = parser.read_markdown_from_file("test/test_document.pdf")
+    print(text[:1000])
+
+
+
+
+
